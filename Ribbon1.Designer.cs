d39962e7d369d4d76c2c5659e406ccee53f13334
tomohiro.mayumi.j5e@jpgr.denso.com <tomohiro.mayumi.j5e@jpgr.denso.com>
2024-12-20 18:01:09 +0900

    一旦作成できた

---
+++ Ribbon1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace csxrunner
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.scriptComboBox = this.Factory.CreateRibbonDropDown();
+            this.runButton = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "tab1";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.scriptComboBox);
+            this.group1.Items.Add(this.runButton);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // scriptComboBox
+            // 
+            this.scriptComboBox.Label = "1";
+            this.scriptComboBox.Name = "scriptComboBox";
+            // 
+            // runButton
+            // 
+            this.runButton.Label = "Run Script";
+            this.runButton.Name = "runButton";
+            this.runButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.RunButton_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.PowerPoint.Presentation";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown scriptComboBox;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton runButton;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ Ribbon1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System.Threading.Tasks;
+
+namespace csxrunner
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            LoadScripts();
+        }
+        private void LoadScripts()
+        {
+            string scriptFolder = @"c:\work\cs\csx";
+            var files = System.IO.Directory.GetFiles(scriptFolder, "*.csx");
+
+            scriptComboBox.Items.Clear(); // 既存のアイテムをクリア
+
+            foreach (var file in files)
+            {
+                string fileName = System.IO.Path.GetFileName(file);
+                var item = this.Factory.CreateRibbonDropDownItem();
+                item.Label = fileName; // ドロップダウンアイテムのラベルを設定
+                scriptComboBox.Items.Add(item); // アイテムを追加
+            }
+        }
+        private async void RunButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            if (scriptComboBox.SelectedItem != null)
+            {
+                string selectedScript = scriptComboBox.SelectedItem.ToString();
+                string scriptPath = System.IO.Path.Combine(@"c:\work\cs\csx", selectedScript);
+
+                // C#スクリプトを実行
+                await ExecuteScript(scriptPath);
+            }
+        }
+
+        private async Task ExecuteScript(string scriptPath)
+        {
+            try
+            {
+                // スクリプトファイルの内容を読み込む
+                string scriptCode = System.IO.File.ReadAllText(scriptPath);
+
+                // スクリプトを実行する
+                var result = await CSharpScript.EvaluateAsync(scriptCode, ScriptOptions.Default);
+
+                // 結果を表示する（必要に応じて）
+                System.Windows.Forms.MessageBox.Show(result?.ToString() ?? "スクリプトが正常に実行されました。");
+            }
+            catch (Exception ex)
+            {
+                // エラーハンドリング
+                System.Windows.Forms.MessageBox.Show($"エラー: {ex.Message}");
+            }
+        }
+    }
+}
